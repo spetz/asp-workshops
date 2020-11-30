b399e3df250e57d98b86aebfe6bdee64c6a0c22f
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2020-11-30 10:15:02 +0100

    routing

--- src/Trill.Api/Startup.cs
+++ src/Trill.Api/Startup.cs
@@ -1,5 +1,7 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Trill.Application;
 using Trill.Infrastructure;
@@ -22,6 +24,27 @@ namespace Trill.Api
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                
+                endpoints.MapGet("api", async context =>
+                {
+                    await context.Response.WriteAsync("Trill API");
+                });
+
+                endpoints.MapGet("api/stories/{storyId:guid}", async context =>
+                {
+                    var storyId = Guid.Parse(context.Request.RouteValues["storyId"].ToString());
+                    if (storyId == Guid.Empty)
+                    {
+                        context.Response.StatusCode = 404;
+                        return;
+                    }
+                    
+                    // Application service -> get story from DB()
+                    // Serialize to JSON
+
+                    context.Response.ContentType = "application/json";
+                    await context.Response.WriteAsync("{}");
+                });
             });
         }
     }
